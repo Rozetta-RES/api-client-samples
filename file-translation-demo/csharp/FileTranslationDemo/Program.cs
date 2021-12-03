b60606ea470a29576aac050886d0ceea4b366a2e
Liu Shengli <s-liu@rozetta.jp>
2021-12-03 17:39:59 +0900

    remove unnecessary files

--- file-translation-demo/csharp/FileTranslationDemo/Program.cs
+++ file-translation-demo/csharp/FileTranslationDemo/Program.cs
@@ -8,17 +8,12 @@ namespace FileTranslationDemo
     {
         static async System.Threading.Tasks.Task Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
             UserInfo.init("config.json");
             FileTranslateFlow flow = new FileTranslateFlow("https://translate.classiii.info/api/v1");
-            //FileTranslateFlow flow = new FileTranslateFlow("http://localhost:5555/v1");
-            T4ooUser t4ooUser = new T4ooUser { orgId = UserInfo.T4OO_ORG_ID, userId = UserInfo.T4OO_USER_ID, password = UserInfo.T4OO_PASSWORD };
             string[] files = { @"C:\mydocuments\morning.docx" };
             string[] langs = { "en" };
             int fieldId = 1;
             bool done = false;
-            //done = await flow.T4ooFlowAsync(t4ooUser, files, langs, fieldId);
-            //Debug.Assert(done);
             ClassiiiUser classiiiUser = new ClassiiiUser {
                 AccessKey = UserInfo.CLASSIII_ACCESS_KEY,
                 SecretKey = UserInfo.CLASSIII_SECRET_KEY,
