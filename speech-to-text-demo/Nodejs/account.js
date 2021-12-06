561251146bf62fa8c561be0dc167640275b24e90
shiiki <c-shiiki@rozetta.jp>
2021-12-06 15:57:21 +0900

    fix config

---
+++ speech-to-text-demo/Nodejs/account.js
@@ -0,0 +1,11 @@
+const envConfigs = {
+  exeEnv: {
+      host: 'domain',
+      authConfig: {
+          accessKey: 'Your accessKey.',
+          secretKey: 'Your secretKey.',
+      }
+  }
+};
+
+module.exports = envConfigs;
