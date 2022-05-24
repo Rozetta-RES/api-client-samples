7bb2e176b29e8758f76a7247ac3f6cdb15557f53
shiiki <c-shiiki@rozetta.jp>
2022-05-24 15:31:36 +0900

    add t3mt customize file

---
+++ text-translation-demo/Nodejs/t3mt-customize/tthreemt-modelstatus-get.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const authUtils = require('./utils/auth-utils');
+
+const path = '/api/v1/training/job/<jid>';
+
+const authConfig = {
+  accessKey: 'YOUR_ACCESS_KEY',
+  secretKey: 'YOUR_SECRET_KEY',
+  nonce: new Date().getTime().toString()
+};
+
+const signature = authUtils.generateSignature(
+  authConfig.nonce,
+  path,
+  authConfig.secretKey,
+);
+
+const config = {
+  method: 'get',
+  url: 'https://translate.rozetta-api.io/api/v1/training/job/<jid>',
+  headers: {
+    accessKey: authConfig.accessKey,
+    signature,
+    nonce: authConfig.nonce,
+    'Content-Type': 'application/json',
+  },
+};
+
+const main = async () => {
+  try {
+    const response = await axios(config)
+    console.log(JSON.stringify(response.data));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
